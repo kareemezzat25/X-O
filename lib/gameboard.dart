957a8e25beced6c57e5f464bfe75f79752fe17bd
kareemezzat <kareemezzat900@gmail.com>
2025-01-12 10:31:03 +0200

    Timergameboard

--- lib/gameboard.dart
+++ lib/gameboard.dart
@@ -1,8 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class GameBoard extends StatelessWidget {
+class GameBoard extends StatefulWidget {
   static const String routeName = "gameBoard";
   const GameBoard({super.key});
+
+  @override
+  State<GameBoard> createState() => _GameBoardState();
+}
+
+class _GameBoardState extends State<GameBoard> {
+  Timer? _timer;
+  int _elapsedSeconds = 0;
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_elapsedSeconds >= 150) {
+        timer.cancel();
+      } else {
+        setState(() {
+          _elapsedSeconds++;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _timer?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +50,35 @@ class GameBoard extends StatelessWidget {
             fit: BoxFit.cover,
             "assets/images/background.png"),
         Scaffold(
-          backgroundColor: Colors.transparent,
-        ),
+            backgroundColor: Colors.transparent,
+            body: SafeArea(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 20),
+                child: Column(
+                  children: [
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.symmetric(vertical: 18),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(44),
+                            color: Colors.white),
+                        child: Text("$_elapsedSeconds",
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w600))),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Text("Player 1’s Turn",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 36,
+                            fontWeight: FontWeight.w700))
+                  ],
+                ),
+              ),
+            )),
       ],
     );
   }
